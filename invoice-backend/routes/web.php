8da9ccf2a67d96630e5f80a5d6d3ffa5569e46e1
Bhupendra Kathayat <bhupendrakathayatop@gmail.com>
2025-04-09 23:34:59 +0545

    added shadcn library

--- invoice-backend/routes/web.php
+++ invoice-backend/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    dd('this is login page');
+})->name('login');
--- invoice-frontend/src/components/HelloWorld.vue
+++ invoice-frontend/src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup>
+import {Button} from "./ui/button/index.js";
 import { ref } from 'vue'
 
 defineProps({
@@ -9,6 +10,7 @@ const count = ref(0)
 </script>
 
 <template>
+  <Button variant="secondary">Button</Button>
   <h1 class="bg-red-600">{{ msg }}</h1>
 </template>
 
